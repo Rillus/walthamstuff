dc68ea93e4e5a0c23ea497686fd9d55c8cd980e8
Riley <riley@dingdigital.co.uk>
2018-11-30 09:48:08 +0000

    adds 'add' route

---
+++ api/application/controllers/add.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class add extends CI_Controller {
+	public function __construct() {
+        parent::__construct();
+
+        $this->load->database();
+
+        header("Access-Control-Allow-Origin: *");
+        header('Content-type: application/json');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, X_FILENAME");
+	}
+
+	public function index() {
+		
+
+		echo 'hi';
+	}
+}
