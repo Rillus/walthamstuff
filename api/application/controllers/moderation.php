625fb2def29c3f556130efcf63dd67ce7214b5af
Riley <riley@dingdigital.co.uk>
2019-01-21 16:47:29 +0000

    tabs to spaces

--- api/application/controllers/moderation.php
+++ api/application/controllers/moderation.php
@@ -2,84 +2,84 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Moderation extends CI_Controller {
-	public function __construct() {
-		parent::__construct();
+    public function __construct() {
+        parent::__construct();
 
-		$this->load->database();
+        $this->load->database();
         $this->load->helper('url');
 
-		// header("Access-Control-Allow-Origin: *");
-		// header('Content-type: application/json');
-		// header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
-		// header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, X_FILENAME");
-	}
+        // header("Access-Control-Allow-Origin: *");
+        // header('Content-type: application/json');
+        // header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
+        // header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, X_FILENAME");
+    }
 
-	public function index() {
-		$data['revisions'] = $this->db->where('status', 0)
-							 ->get('revisions');
+    public function index() {
+        $data['revisions'] = $this->db->where('status', 0)
+                             ->get('revisions');
 
-	 	$data['news'] = $this->db->where('status', 1)
-							 ->get('locations');
+        $data['news'] = $this->db->where('status', 1)
+                             ->get('locations');
 
-		$this->load->view('revisions', $data);
-	}
+        $this->load->view('revisions', $data);
+    }
 
-	public function approve($id = null) {
-		if ($id) {
-			$thisEntry = $this->db->where('id', $id)
-								  ->get('revisions', 1);
+    public function approve($id = null) {
+        if ($id) {
+            $thisEntry = $this->db->where('id', $id)
+                                  ->get('revisions', 1);
 
-			if ($thisEntry->num_rows() > 0) {
-				$thisEntry = $thisEntry->row();
+            if ($thisEntry->num_rows() > 0) {
+                $thisEntry = $thisEntry->row();
 
-				$this->db->where('id', $thisEntry->location_id);
-				unset($thisEntry->id);
-				unset($thisEntry->status);
-				unset($thisEntry->location_id);
-				unset($thisEntry->timestamp);
-				unset($thisEntry->lon);
-				unset($thisEntry->lat);
-				$this->db->update('locations', $thisEntry);	
+                $this->db->where('id', $thisEntry->location_id);
+                unset($thisEntry->id);
+                unset($thisEntry->status);
+                unset($thisEntry->location_id);
+                unset($thisEntry->timestamp);
+                unset($thisEntry->lon);
+                unset($thisEntry->lat);
+                $this->db->update('locations', $thisEntry); 
 
-				$data['status'] = '1';
+                $data['status'] = '1';
 
-				$this->db->where('id', $id)
-						 ->update('revisions', $data);
-			} else {
-				echo 'error: couldn\'t find location';
-			}
-		} else {
-			echo 'error: no location id supplied';
-		}
-		redirect('moderation');
-	}
+                $this->db->where('id', $id)
+                         ->update('revisions', $data);
+            } else {
+                echo 'error: couldn\'t find location';
+            }
+        } else {
+            echo 'error: no location id supplied';
+        }
+        redirect('moderation');
+    }
 
-	public function publish($id = null) {
-		if ($id) {
-			$thisEntry = $this->db->where('id', $id)
-								  ->get('locations', 1);
+    public function publish($id = null) {
+        if ($id) {
+            $thisEntry = $this->db->where('id', $id)
+                                  ->get('locations', 1);
 
-			if ($thisEntry->num_rows() > 0) {
-				$data['status'] = '0';
+            if ($thisEntry->num_rows() > 0) {
+                $data['status'] = '0';
 
-				$this->db->where('id', $id)
-						 ->update('locations', $data);
-			} else {
-				echo 'error: couldn\'t find location';
-			}
-		} else {
-			echo 'error: no location id supplied';
-		}
-		redirect('moderation');
-	}
+                $this->db->where('id', $id)
+                         ->update('locations', $data);
+            } else {
+                echo 'error: couldn\'t find location';
+            }
+        } else {
+            echo 'error: no location id supplied';
+        }
+        redirect('moderation');
+    }
 
-	public function reject($id = null) {
-		if ($id) {
-			$this->db->where('id', $id)
-				     ->delete('revisions');
-		} else {
-			echo 'error: no location id supplied';
-		}
-		redirect('moderation');
-	}
+    public function reject($id = null) {
+        if ($id) {
+            $this->db->where('id', $id)
+                     ->delete('revisions');
+        } else {
+            echo 'error: no location id supplied';
+        }
+        redirect('moderation');
+    }
 }
--- api/application/views/revisions.php
+++ api/application/views/revisions.php
@@ -2,40 +2,40 @@
 
 <h2>Newly added locations</h2>
 <?php if ($news->num_rows() > 0) { ?>
-	<ul>
-	<?php foreach($news->result() as $new) { ?>
-		<li>
-			<?php echo $new->name; ?><br>
-			<?php echo $new->description; ?><br>
-			<?php echo $new->address; ?><br>
-			<?php echo $new->category; ?><br>
-			<a href="<?php echo base_url('/edit.html?id='.$new->id); ?>">Edit</a> - <a href="<?php echo site_url('moderation/publish/'.$new->id); ?>">Publish</a> - <a href="<?php echo site_url('moderation/reject/'.$new->id); ?>">Reject</a>
-		</li>
-		<?php 
-	} ?>
-	</ul>
-	<?php 
+    <ul>
+    <?php foreach($news->result() as $new) { ?>
+        <li>
+            <?php echo $new->name; ?><br>
+            <?php echo $new->description; ?><br>
+            <?php echo $new->address; ?><br>
+            <?php echo $new->category; ?><br>
+            <a href="<?php echo base_url('/edit.html?id='.$new->id); ?>">Edit</a> - <a href="<?php echo site_url('moderation/publish/'.$new->id); ?>">Publish</a> - <a href="<?php echo site_url('moderation/reject/'.$new->id); ?>">Reject</a>
+        </li>
+        <?php 
+    } ?>
+    </ul>
+    <?php 
 } else { ?>
-	<p>No locations requiring approval</p>
-	<?php 
+    <p>No locations requiring approval</p>
+    <?php 
 } ?>
 
 <h2>Edited locations</h2>
 <?php if ($revisions->num_rows() > 0) { ?>
-	<ul>
-	<?php foreach($revisions->result() as $revision) { ?>
-		<li>
-			<?php echo $revision->name; ?><br>
-			<?php echo $revision->description; ?><br>
-			<?php echo $revision->address; ?><br>
-			<?php echo $revision->category; ?><br>
-			<a href="<?php echo base_url('/edit.html?id='.$revision->location_id); ?>">Edit</a> - <a href="<?php echo site_url('moderation/approve/'.$revision->id); ?>">Approve</a> - <a href="<?php echo site_url('moderation/reject/'.$revision->id); ?>">Reject</a>
-		</li>
-		<?php 
-	} ?>
-	</ul>
-	<?php 
+    <ul>
+    <?php foreach($revisions->result() as $revision) { ?>
+        <li>
+            <?php echo $revision->name; ?><br>
+            <?php echo $revision->description; ?><br>
+            <?php echo $revision->address; ?><br>
+            <?php echo $revision->category; ?><br>
+            <a href="<?php echo base_url('/edit.html?id='.$revision->location_id); ?>">Edit</a> - <a href="<?php echo site_url('moderation/approve/'.$revision->id); ?>">Approve</a> - <a href="<?php echo site_url('moderation/reject/'.$revision->id); ?>">Reject</a>
+        </li>
+        <?php 
+    } ?>
+    </ul>
+    <?php 
 } else { ?>
-	<p>No locations requiring approval</p>
-	<?php 
+    <p>No locations requiring approval</p>
+    <?php 
 } ?>
