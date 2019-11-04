0626695e50d76a4c32e68b17f62da077b8d75f7a
Antonia Cronin <antoniajcronin@gmail.com>
2019-11-04 17:02:30 +0000

    Add React to the application

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+
+class App extends Component{
+   
+   render(){
+      return(
+         <Fragment>
+            <div>
+               <h1>Hello World!!!</h1>
+            </div>
+         </Fragment>
+      );
+   }
+}
+export default App;
