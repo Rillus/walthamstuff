458b94a652f66c09488c6548d0d43683dcf70437
Riley <riley@dingdigital.co.uk>
2019-11-11 17:37:56 +0000

    Update scripts/common.js

--- scripts/common.js
+++ scripts/common.js
@@ -65,7 +65,6 @@ function sendData(e) {
     // Define what happens on successful data submission
     XHR.addEventListener('load', function(event) {
         //alert(body);
-        //alert(object.data.venueId);
         id = JSON.parse(XHR.responseText).data.venueId;
         window.location.assign('venue.html?id=' + id);
     });
