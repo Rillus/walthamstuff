638b4a530319c187431ddb6856405cd01a75aeac
Riley <riley@dingdigital.co.uk>
2019-11-12 08:54:26 +0000

    fixes bug where venue wasn't highlighted on hovering venue name on left side

--- scripts/maps.js
+++ scripts/maps.js
@@ -174,20 +174,32 @@ function createCategoryList() {
 function highlightVenue(e) {
     setMarkers(null);
 
-    var thisVenueId = e.target.attributes['data-venueid'].value;
-    var thisVenue = $.grep(venues, function(e){ return e.id == thisVenueId; });
+    var thisVenueId;
 
-    thisVenue = thisVenue[0];
+    // if the class 'Venues-listItemDescription' is on the target element then we're on a child, so navigate up one to get the id
+    if ($(e.target).hasClass('Venues-listItemDescription')) {
+        thisVenueId = e.target.parentNode.attributes['data-venueid'].value;
+    } else {
+        // if that class isn't on here, we're already on the parent
+        thisVenueId = e.target.attributes['data-venueid'].value;
+    }
+    
+    if (thisVenueId !== null) {
 
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(thisVenue.lat, thisVenue.lon),
-        map: map
-    });
+        var thisVenue = $.grep(venues, function(e){ return e.id == thisVenueId; });
 
-    markers = [];
-    markers.push(marker);
+        thisVenue = thisVenue[0];
 
-    setMarkers(map);
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(thisVenue.lat, thisVenue.lon),
+            map: map
+        });
+
+        markers = [];
+        markers.push(marker);
+
+        setMarkers(map);
+    }
 }
 
 function rollOffVenue() {
