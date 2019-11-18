3bc4edae867d98438ae5af32a9e7b118192a96b9
Antonia Cronin <antoniajcronin@gmail.com>
2019-11-18 19:26:04 +0000

    Change add and edit files to php, use includes

---
+++ add.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Add a new location to Walthamstuff Map</title>
+    <link rel="stylesheet" href="stylesheets/bootstrap.min.css">
+    <link rel="stylesheet" href="stylesheets/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <script src="scripts/add.js"></script>
+    <script src="scripts/common.js"></script>
+    <script src="scripts/bootstrap.min.js"></script>
+  </head>
+  <body>
+  <?php include "./includes/admin_header.php" ?>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+      <?php include "./includes/form.php" ?>
+      <?php include "./includes/form.php" ?>
+      </div>
+    </div>
+  </div>
+    </body>
+</html>
