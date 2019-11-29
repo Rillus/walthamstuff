4cb985ccba5e1dbec96b827b1ea6afacb086edbc
Antonia Cronin <antoniajcronin@gmail.com>
2019-11-29 13:52:43 +0000

    Add class to back button

--- venue.php
+++ venue.php
@@ -46,7 +46,7 @@
 
 
       <div class="container adjacent-footer">
-      <button class="btn btn-default" onclick="goBack()" type="button">< Back to Search</button>
+      <button class="btn btn-default small-text" onclick="goBack()" type="button" id="back-button">< Back to Search</button>
       <section class="VenueDetails">
         <div class="VenueDetails-hero">
             <div id="map_canvas" class="Map--venue"></div>
@@ -54,6 +54,10 @@
                     <h1 class="VenueDetails-title"></h1>
                     <div class="VenueDetails-address u-hide">
                         <p class="VenueDetails-addressContent"></p>
+                    </div>
+                    <div class="VenueDetails-opening_times u-hide">
+                        <h3>Opening Times</h3>
+                        <p class="VenueDetails-opening_timesContent"></p>
                     </div>
                 </div>
                 <!-- <div class="VenueDetails-dateprice">
@@ -67,17 +71,13 @@
             </div>
 
 
-            <div class="VenueDetails-openingTimes u-hide">
-                <h3>Opening Times</h3>
-                <p class="VenueDetails-openingTimesContent"></p>
-            </div>
 
             <div class="VenueDetails-description u-hide">
                 <h3>Description</h3>
                 <p class="VenueDetails-descriptionContent"></p>
             </div>
-
-            <!-- <div class="VenueDetails-images">
+<!-- 
+            <div class="VenueDetails-images">
                 <div class="VenueDetails-image" style="background-image:url(https://www.wmgallery.org.uk/media/w770/generic/WMG_reception.jpg);"></div>
                 <div class="VenueDetails-image" style="background-image:url(https://missgen.com/wp-content/uploads/2017/04/002-william-morris-gallery-wedding-photographer.jpg);"></div>
                 <div class="VenueDetails-image" style="background-image:url(https://go.skimresources.com/?id=71026X1533819&xs=1&isjs=1&url=https%3A%2F%2Fhitchedukir.hitched.co.uk%2FTemp%2F1000_1000_scaled_1989322_william-morr-20180130112206502.jpg&xguid=01CYPDNZ6TPBXG3KEYTZMKHQT6&xuuid=a3cd7ea24ae71cd0345940b016468095&xsessid=&xcreo=0&xed=0&sref=https%3A%2F%2Fwww.hitched.co.uk%2Fwedding-venues%2Fwilliam-morris-gallery_7792.htm&pref=https%3A%2F%2Fwww.google.co.uk%2F&xtz=-60&jv=13.15.1-stackpath&bv=2.5.1);"></div>
